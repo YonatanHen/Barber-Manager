ae0d70f99b2388625138c8830b66f27a0c81e82e
Yehonatan Hen <yehone@ac.sce.ac.il>
2020-07-19 16:06:08 +0300

    Add project files.

---
+++ barberManager/mainMenu.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace barberManager
+{
+    /// <summary>
+    /// Interaction logic for mainMenu.xaml
+    /// </summary>
+    public partial class mainMenu : Page
+    {
+        private object _content;
+        private MainWindow mainWindow;
+        public mainMenu(object _content,MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            this._content = _content;
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mainWindow.Content = _content;
+        }
+    }
+}
---
+++ barberManager/user.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barberManager
+{
+    class User : Person
+    {
+        private string uname;
+        private string password;
+        public User(string password, string uname):base()
+        {
+            this.password = password;
+            this.uname = uname;
+        }
+        string Password { get; set; } //NOTE: access it with objectname.Password
+        string Uname { get; set; }
+
+    }
+}
